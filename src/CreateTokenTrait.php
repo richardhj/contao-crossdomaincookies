068785d16f19b5b31139c6cb9902b5d21bb8a06f
Richard Henkenjohann <richardhenkenjohann@googlemail.com>
2017-09-11 23:24:13 +0200

    PHPDoc

--- src/CreateTokenTrait.php
+++ src/CreateTokenTrait.php
@@ -25,16 +25,32 @@ use Contao\Environment;
 trait CreateTokenTrait
 {
 
+    /**
+     * @param int $userId
+     *
+     * @return string
+     */
     private function createIncludeToken($userId)
     {
         return $this->createToken($userId, 'include');
     }
 
+    /**
+     * @param int $userId
+     *
+     * @return string
+     */
     private function createLinkToken($userId)
     {
         return $this->createToken($userId, 'link');
     }
 
+    /**
+     * @param int    $userId
+     * @param string $action
+     *
+     * @return string
+     */
     private function createToken($userId, $action)
     {
         $ip = (!Config::get('disableIpCheck')) ? Environment::get('ip') : '';
