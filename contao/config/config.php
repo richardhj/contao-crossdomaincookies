66fd5c7cc75e2dd2f852b8428d6086512dad9079
Richard Henkenjohann <richardhenkenjohann@googlemail.com>
2017-09-12 10:11:35 +0200

    Fix LICENSE link

--- contao/config/config.php
+++ contao/config/config.php
@@ -8,7 +8,7 @@
  * @package   CrossDomainCookies
  * @author    Richard Henkenjohann <richardhenkenjohann@googlemail.com>
  * @copyright 2017 Richard Henkenjohann
- * @license   https://github.com/MetaModels/attribute_translatedtabletext/blob/master/LICENSE LGPL-3.0
+ * @license   https://github.com/richardhj/contao-crossdomaincookies/blob/master/LICENSE LGPL-3.0
  */
 
 
